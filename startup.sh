b7f70cd2e01c0c736c996dc7d63a6cd3b3136be8
root <root@cv4280499.novalocal>
2024-11-23 23:30:19 +0300

    update for git

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-python manage.py collectstatic
+
+git pull origin main
+source .venv/bin/activate
+pip install -r requirements.txt
+yes yes | python manage.py collectstatic
 python manage.py migrate
 sudo systemctl restart gunicorn
