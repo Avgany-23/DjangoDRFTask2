cd8e3da6e8f36b721d2d4ece5aebb0c8c1ea4432
root <root@cv4280499.novalocal>
2024-11-24 13:50:00 +0300

    update config deploy16

--- startup.sh
+++ startup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-apt install -y git
 cd /root/project
 git pull origin main
 source .venv/bin/activate
 pip install -r requirements.txt
-yes yes | python manage.py collectstatic
+python manage.py collectstatic
 python manage.py migrate
 sudo systemctl restart gunicorn
 
